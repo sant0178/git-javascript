e47a659b6128ba201186c3c63a3e4a2dd1897356
JS50 <sant0178@algonquinlive.com>
2020-01-13 17:50:03 -0500

    Added html and js

---
+++ script.js
@@ -0,0 +1,19 @@
+const name = 'Jose'
+
+console.log(`Hello my name is ${name}.`)
+
+let number = 4
+let multi = number * 2
+
+console.log(`The number ${number} multiplied by 2 is ${multi} `)
+
+let multi2 = multi * 5
+
+console.log(`The result of ${multi} multiplied by 5 is ${multi2} `)
+
+let div = multi2 / number
+
+console.log(`The result of ${multi2} divided by ${number} is ${div} `)
+
+let sub = div - 7
+console.log(`The result of ${div} subtracted by 7 is ${sub} `)
